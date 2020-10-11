f476ce0a89bd0c323dad986b96ac5b9091ec475b
Joe Wildfong <eric.joe.wildfong@gmail.com>
2020-10-11 19:35:37 +0000

    Add new test frames.

--- data.js
+++ data.js
@@ -2,5 +2,10 @@ const allFrames = {
   "joe": {
     "sub 1": [false, true],
     "sub 2": [false, true, true, false]
+  },
+  "joe2": {
+    "sub 1:": [false],
+    "sub 2": [false, false, false, false, true, false, false],
+    "sub 3": [true]
   }
 }
