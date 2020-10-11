7ca7d6b154a874a4ba9ee78a84202e7590ba07be
repl.it user <replituser@example.com>
2020-10-11 23:44:21 +0000

    Add necessary PWA files

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 !function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', { scope: '/Umbrella-Script/' }).then(function(reg) {
+
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
+
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  } else {
+    console.log("Service worker unsupported");
+  }
   const MSPERFRAME = 1000;
   const BLANKFRAMES = 3;
   const OPENCOLOR = "green";
--- data.js
+++ data.js
@@ -3,8 +3,8 @@ const allFrames = {
     "sub 1": [false, true],
     "sub 2": [false, true, true, false]
   },
-  "joe2": {
-    "sub 1:": [false],
+  "test": {
+    "sub 1": [false],
     "sub 2": [false, false, false, false, true, false, false],
     "sub 3": [true]
   }
